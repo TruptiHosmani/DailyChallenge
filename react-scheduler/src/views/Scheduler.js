b5e83e49685736a1ffa4f92eab37bf19983438b0
Trupti Hosamani <trupti.hosamani7@gmail.com>
2020-02-11 12:26:02 +0530

    moved task list and schedulr into different files

---
+++ react-scheduler/src/views/Scheduler.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Calendar from 'react-calendar'
+import { FaCalendarAlt } from 'react-icons/fa';
+
+function Scheduler(props) {
+    const list = props.list
+    const setList = props.setList
+    const item = props.item
+    const closeAddTime = props.closeAddTime
+    const date = props.date
+
+    const changeSelectedDate = (date) => {
+        if (item.name) {
+            const ele = list.filter(ele => { return item.name === ele.name })
+            const rest = list.filter(ele => { return item.name !== ele.name })
+            ele[0].date = date.toISOString().slice(0, 10);
+            setList([...rest, ele[0]])
+
+        }
+
+    }
+    const changeTime = (time) => {
+        if (item.name) {
+            const ele = list.filter(ele => { return item.name === ele.name })
+            const rest = list.filter(ele => { return item.name !== ele.name })
+            ele[0].time = time
+            setList([...rest, ele[0]])
+        }
+
+    }
+    return (
+
+        <div className="container">
+            <div className="item" >  <input type="text" className="inputWithIcon" placeholder="00:00" value={item.time} onChange={(e) => changeTime(e.target.value)} />
+                <FaCalendarAlt className="listIcon calenderIcon" size="24" />
+            </div>
+            <div className="item" >
+                <Calendar
+                    className="calender"
+                    onChange={changeSelectedDate}
+                    value={date}
+                />
+            </div>
+            <div className="item" >
+                <div className="item"><button className="button green" onClick={closeAddTime}>Set Date</button>
+
+                    <button className="button red" onClick={closeAddTime}>x</button></div>
+            </div>
+        </div>
+    );
+}
+
+export default Scheduler;
---
+++ react-scheduler/src/views/TaskList.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { MdKeyboardArrowLeft, MdApps, MdList, MdTimer } from 'react-icons/md'
+import { AiOutlineClockCircle, AiOutlineCaretRight, AiOutlineCaretDown } from 'react-icons/ai'
+
+
+function TaskList(props) {
+    const toggleList = props.toggleList
+    const handleSelectTask = props.handleSelectTask
+    const list = props.list
+    const [selectedIndex, setSelectIndex] = useState('')
+    const [isShowDesc, setShowDesc] = useState(false)
+    return (
+        <div className="container">
+
+            <div className="item">
+
+                <MdApps className="appsIcon" size="34" />
+                <label className="text">Mike</label>
+                <AiOutlineClockCircle className="listIcon" size="34" onClick={toggleList} />
+            </div>
+
+            <span className="spanWithHr">Todays Activity</span><hr className="hr-strike" />
+            <ul>
+                {list.filter(ele => { return ele.type === "important" }).map((item, index) => {
+                    return (
+                        <li className={item.type} key={index} >
+                            <label onClick={(e) => handleSelectTask(item)}>{item.name}</label>
+                            {isShowDesc && item.id.toString() == selectedIndex ? <AiOutlineCaretDown className="listIcon" onClick={(e) => { setShowDesc(!isShowDesc); setSelectIndex(item.id); console.log(item.id) }} /> :
+                                <AiOutlineCaretRight className="listIcon" onClick={(e) => { setShowDesc(!isShowDesc); setSelectIndex(item.id); console.log(item.id) }} />}
+                            <br />
+                            {isShowDesc && item.id.toString() == selectedIndex ? <small>Task Description</small> : null}
+
+                        </li>
+                    )
+                })}
+            </ul>
+            <span className="spanWithHr">Tasks</span><hr className="hr-strike" />
+
+            <ul>
+                {list.filter(ele => { return ele.type !== "important" }).map((item, index) => {
+                    return (
+                        <li className={item.type} key={index} >
+                            <label onClick={(e) => handleSelectTask(item)}>{item.name}</label>
+                            {isShowDesc && item.id.toString() == selectedIndex ? <AiOutlineCaretDown className="listIcon" onClick={(e) => { setShowDesc(!isShowDesc); setSelectIndex(item.id); console.log(item.id) }} /> :
+                                <AiOutlineCaretRight className="listIcon" onClick={(e) => { setShowDesc(!isShowDesc); setSelectIndex(item.id); console.log(item.id) }} />}
+                            <br />
+                            {isShowDesc && item.id.toString() == selectedIndex ? <small>Task Description</small> : null}
+
+                        </li>
+                    )
+                })}
+
+            </ul>
+        </div>
+
+    );
+}
+
+export default TaskList;
