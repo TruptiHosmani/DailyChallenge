f3098c7adf8e446ba2a4d24a923636bb2a283914
Trupti Hosamani <trupti.hosamani7@gmail.com>
2020-02-12 09:21:41 +0530

    converted time into % for svg circle

--- react-scheduler/src/App.js
+++ react-scheduler/src/App.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
 import { MdApps, MdFormatListBulleted, MdTimer } from 'react-icons/md'
-import Timer from './Timer'
 import Scheduler from './views/Scheduler'
 import TaskList from './views/TaskList'
 import moment from 'moment'
+import Circle from 'react-circle';
+
 function App() {
-  const [item, setItem] = useState({})
 
   const [isCalenderDivVisible, toggleCalenderDiv] = useState(false)
   const [list, setList] = useState([
@@ -18,6 +18,7 @@ function App() {
     { id: 6, name: 'Design Work', date: '', time: "00.01", type: "normal" },
     { id: 7, name: 'Design Dashboard', date: '', time: "00.10", type: "mid" },
     { id: 8, name: 'Development Work', date: '', time: "00.00", type: "normal" }])
+  const [item, setItem] = useState(list[0])
 
   const [isListDivVisible, toggleListDiv] = useState(false)
 
@@ -58,8 +59,32 @@ function App() {
           <input type="text" value={item.name} placeholder="Type Task Name" onFocus={handleSelectTask} />
 
         </div>
-        <div className="item" >
-          <Timer time={item.time || ""} />
+        <div className="item" style={{ justifyContent: "space-evenly", }} >
+          {/* <Timer time={item.time || ""} /> */}
+          <Circle
+
+            animate={true} // Boolean: Animated/Static progress
+            animationDuration="1s" // String: Length of animation
+            responsive={false} // Boolean: Make SVG adapt to parent size
+            size="250" // String: Defines the size of the circle.
+            lineWidth="2" // String: Defines the thickness of the circle's stroke.
+            progress={item.time ? parseInt(item.time.split(".")[1]) / 60 * 100 : 0} // String: Update to change the progress and percentage.
+            progressColor="red" // String: Color of "progress" portion of circle.
+            bgColor="#ecedf0" // String: Color of "empty" portion of circle.
+            textColor="#6b778c" // String: Color of percentage text color.
+            textStyle={{
+              font: 'bold 4rem Helvetica, Arial, sans-serif' // CSSProperties: Custom styling for percentage.
+            }}
+            percentSpacing={10} // Number: Adjust spacing of "%" symbol and number.
+            roundedStroke={false} // Boolean: Rounded/Flat line ends
+            showPercentage={false} // Boolean: Show/hide percentage.
+            showPercentageSymbol={false} // Boolean: Show/hide only the "%" symbol.
+
+          />
+          <text style={{ marginTop: "90px", left: "-180px", position: "relative" }}>
+            <span style={{ fontSize: "20px" }}>{item.time}</span><br />
+            < MdTimer size="24" color="red" onClick={openAddTime} /></text>
+
         </div>
         <div className="item" >
           <input type="text" className="inputWithIcon" value={dateValue} placeholder="Add time" onChange={openAddTime} onFocus={openAddTime} />
